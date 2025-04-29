35520ce1ecc84f1a1d954dbba2f9685b51f7264c
WASD-6923 <r23368460@gmail.com>
2025-04-29 18:45:59 +0500

    empity auth
    добавил ручки пока что в мейне,ни в коем случае не пишить в этот репозиторий SECRET_KEY на то он и секретный

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.routers.router import router as router_anime
 from pydantic import BaseModel
 from typing import List, Annotated
+from authx import AuthX, AuthXConfig
 
 app = FastAPI()
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "SECRET_KEY"
+config.JWT_ACCESS_COOKIE_NAME = "my_acess_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = Auth(config=config)
 
 class User_Base(BaseModel):
   user_name: str
@@ -22,6 +30,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.post("/login")
+async def login():
+  pass
+
+@app.get("/protected")
+async def protected():
+  pass
 
 
 app.include_router(router_anime)
